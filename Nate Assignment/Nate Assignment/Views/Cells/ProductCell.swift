2aa9f12b8c0d2041e6bb08182482260b90cb0e17
Kali Spassov <kali_spassov@yahoo.com>
2022-07-10 16:25:37 +0300

    Added cell image rotation.

--- Nate Assignment/Nate Assignment/Views/Cells/ProductCell.swift
+++ Nate Assignment/Nate Assignment/Views/Cells/ProductCell.swift
@@ -10,6 +10,7 @@ import SDWebImage
 
 class ProductCell: UICollectionViewCell {
     static let identifier = "ProductCell"
+    private static let noImagePlaceholder = "no_image"
     
     struct ViewModel {
         let images: [String]
@@ -21,16 +22,61 @@ class ProductCell: UICollectionViewCell {
     @IBOutlet weak var topText: UILabel!
     @IBOutlet weak var botText: UILabel!
     
+    var imageNumber = 0
+    var shouldRefreshImage = false
     
     var viewModel: ViewModel? {
         didSet {
             guard let viewModel = viewModel else { return }
             if let firstImage = viewModel.images.first, let url = URL(string: firstImage) {
-            image.sd_setImage(with: url)
+                image.sd_setImage(with: url, placeholderImage: UIImage(named: ProductCell.noImagePlaceholder))
+                
+                setupImageRotation()
+            } else {
+                image.image = UIImage(named: ProductCell.noImagePlaceholder)
             }
             
             topText.text = viewModel.topTitle
             botText.text = viewModel.botTitle
         }
+    }
+    
+    private func setupImageRotation() {
+        guard let viewModel = viewModel,
+        viewModel.images.count > 1 else {
+            return
+        }
+        shouldRefreshImage = true
+        imageRotation()
+    }
+    
+    private func imageRotation() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            guard let self = self,
+                  let viewModel = self.viewModel,
+                  self.shouldRefreshImage else {
+                return
+            }
+            
+            if self.imageNumber >= viewModel.images.count - 1 {
+                self.imageNumber = 0
+            } else {
+                self.imageNumber += 1
+            }
+            if let nextImage = viewModel.images[safe: self.imageNumber],
+               let nextImageUrl = URL(string: nextImage) {
+                self.image.sd_setImage(with: nextImageUrl, placeholderImage: UIImage(named: ProductCell.noImagePlaceholder))
+            }
+            self.imageRotation()
+        }
+    }
+    
+    private func endImageRotation() {
+        shouldRefreshImage = false
+    }
+    
+    override func prepareForReuse() {
+        endImageRotation()
+        super.prepareForReuse()
     }
 }
